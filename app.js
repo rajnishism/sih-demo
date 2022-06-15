2de8245a21edd0d6e8652cdf0a2dda50f4d565a0
rajnishism <starajnish8@gmail.com>
2022-06-15 18:16:25 +0530

    jdfj

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+var router = express.Router();
+
+
+
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local');
+// var crypto = require('crypto');
+//
+// var logger = require('morgan');
+// var passport = require('passport');
+// var session = require('express-session');
+//
+// var SQLiteStore = require('connect-sqlite3')(session);
+//
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: false,
+//   store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+// }));
+// app.use(passport.authenticate('session'));
 
 //multer
 const multer = require("multer");
@@ -17,10 +38,61 @@ const storage = multer.diskStorage({
 
 var upload = multer({ storage: storage })
 
+
+
+//passport
+
+// passport.use(new LocalStrategy(function verify(username, password, cb) {
+//   db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+//     if (err) { return cb(err); }
+//     if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+//
+//     crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//       if (err) { return cb(err); }
+//       if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+//         return cb(null, false, { message: 'Incorrect username or password.' });
+//       }
+//       return cb(null, row);
+//     });
+//   });
+// }));
+//
+//
+// app.post('/login/password', passport.authenticate('local', {
+//   successRedirect: '/',
+//   failureRedirect: '/login'
+// }));
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname +'/public'));
 app.use(bodyParser.json())
+
+
+// passport.serializeUser(function(user, cb) {
+//   process.nextTick(function() {
+//     cb(null, { id: user.id, username: user.username });
+//   });
+// });
+//
+// passport.deserializeUser(function(user, cb) {
+//   process.nextTick(function() {
+//     return cb(null, user);
+//   });
+// });
+
+
+// router.post('/logout', function(req, res, next) {
+//   req.logout(function(err) {
+//     if (err) { return next(err); }
+//     res.redirect('/');
+//   });
+// });
+
+
+// router.get('/signup', function(req, res, next) {
+//   res.render('signup');
+// });
 
 // database save
 const mongoose = require('mongoose');
@@ -60,8 +132,9 @@ app.get('/',(req,res)=>{
 
   Tender.find({}, (err, recentTender)=>{
     Article.find({}, (err, recentArticle)=>{
-
-      res.render('index',{ recentTender: recentTender , recentArticle:recentArticle,  nArticle: recentArticle.length, nTender:recentTender.length, })
+      Project.find({}, (err, recentProject)=>{
+        res.render('index',{  recentProject: recentProject, recentTender: recentTender , recentArticle:recentArticle,  nArticle: recentArticle.length, nTender:recentTender.length, nProject:recentProject.length})
+      })
 
     })
 
@@ -216,10 +289,20 @@ app.get('/projects/:title',(req,res)=>{
   Project.findOne({title: title},(err, found)=>{
     Tender.find({}, (err, recentTender)=>{
       Article.find({}, (err, recentArticle)=>{
-        console.log(found.imglink[0]);
+        console.log(found.imglink);
+
+        res.render('singlePageProject',{
+          imglink: found.imglink,
+          nImgLink: found.imglink.length,
+          title: title, body: found.body,
+          imglink: found.imglink,
+          recentTender: recentTender ,
+          recentArticle:recentArticle,
+          nArticle: recentArticle.length,
+          nTender:recentTender.length,
 
 
-        res.render('singlePageProject',{  imglink: found.imglink[0], nImgLink: found.imglink.length, title: title, body: found.body, imglink: found.imglink,  recentTender: recentTender , recentArticle:recentArticle,  nArticle: recentArticle.length, nTender:recentTender.length, })
+          })
 
 
       })
@@ -268,7 +351,13 @@ app.get('/contact',(req,res)=>{
 
 //dashboard
 app.get('/dashboard',(req,res)=>{
-  res.render('dashboard')
+  Tender.find({}, (err, tenders)=>{
+    Article.find({}, (err, articles)=>{
+      Project.find({},(err, projects)=>{
+        res.render('dashboard',{tenders: tenders, projects:projects, articles: articles, nArticle:articles.length, nTender: tenders.length , nProject: projects.length })
+      })
+    })
+  })
 });
 
 app.get('/dashboard/tenders',(req,res)=>{
@@ -277,6 +366,10 @@ app.get('/dashboard/tenders',(req,res)=>{
 
 app.get('/dashboard/create-project',(req, res)=>{
   res.render('composeProject')
+})
+
+app.get('/dashboard/create-article',(req,res)=>{
+  res.render('composeArticle')
 })
 
 // individual tender page
@@ -306,6 +399,27 @@ app.get('/tenders/:title',(req,res)=>{
 });
 
 app.get('/about',(req,res)=>{
+
+})
+
+app.post('/articles', (req,res)=>{
+
+  console.log(req.body.title);
+  console.log(req.body.body);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    body: req.body.body,
+
+  })
+  console.log(req.body);
+  newArticle.save(function(err) {
+  if (!err)
+    res.send("success");
+  else {
+    res.send(err);
+  }
+  });
 
 })
 
@@ -358,6 +472,36 @@ app.post('/projects',upload.array('imglink') ,(req,res,next)=>{
   }
   });
 
+})
+
+app.post('/projects/:title', (req,res)=>{
+  var title= req.params.title;
+  Project.deleteOne({title: title}, (err, done)=>{
+    if(err)
+    console.log(err);
+    res.send("1 document deleted");
+  })
+})
+
+app.post('/articles/:title', (req, res)=>{
+
+  var title= req.params.title;
+  Article.deleteOne({title: title}, (err, done)=>{
+    if(err)
+    console.log(err);
+    res.send("1 Article deleted");
+  })
+})
+
+
+app.post('/tenders/:title', (req, res)=>{
+
+  var title= req.params.title;
+  Tender.deleteOne({title: title}, (err, done)=>{
+    if(err)
+    console.log(err);
+    res.send("1 Tender deleted");
+  })
 })
 
 
